1908af8fee7992c2d4223bc10045212ac789d755
zhanchi5 <zhanchi5@outlook.com>
2016-11-11 12:59:39 +0300

    Add files via upload

---
+++ bmstu_lab4_1.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctime>
+
+int main()
+{
+	srand(time(0));
+        double *lMassive;
+        int n; // размерность
+        std::cout << "Enter size of array" <<std::endl;
+        std::cin >> n;
+        lMassive = (double *)malloc(n*sizeof(double)); // выделяем пам€ть динамически
+        // Заполн€ем массив случайными числами
+        for (int i=0; i<n; i++)
+        {
+                lMassive[i] = rand() %100;
+                std::cout << "a["<<i<<"]="<<lMassive[i] << std::endl;
+        }
+
+
+		// найти произведение элементов массива с нечетными номерами
+          int p=1;
+          for (int i=1; i<n; i+=2)
+          {
+            p*=lMassive[i];
+          }
+
+          std::cout << "answer: " << p << std::endl;
+
+
+
+        free(lMassive);
+        system("Pause");
+}
