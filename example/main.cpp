ff379768ea3b7e5dd41d41d74cde10ed698a0800
zhanchi5 <zhanchi5@outlook.com>
2016-11-16 14:13:13 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,14 @@
+
+#include "stdafx.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.hpp"
+
+using namespace std;
+
+int main()
+{
+  cout << "summary = " << summary <<endl;
+  return 0;
+}
