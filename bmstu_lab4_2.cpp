0f8b0e8f03b38a56c2461487878215ba03cc302f
zhanchi5 <zhanchi5@outlook.com>
2016-11-29 10:48:39 +0300

    Update bmstu_lab4_2.cpp

--- bmstu_lab4_2.cpp
+++ bmstu_lab4_2.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	
-	int n ;
-	int m ;
-	int count_row;
-	int count_column;
+	unsigned int n ;
+	unsigned int m ;
+	unsigned int count_row;
+	unsigned int count_column;
 
 
 	cout << "Enter value of rows" << endl;
@@ -22,38 +22,32 @@ int main(int argc, char* argv[])
 	int **Array = new int*[n]; 
 	
 	
-	for (int count = 0; count < n; count++)
+	for (unsigned int count = 0; count < n; ++count)
 		Array[count] = new int[m]; 
 										
-	for (int count_row = 0; count_row < n; count_row++)
-		for (int count_column = 0; count_column < m; count_column++)
+	for (unsigned int count_row = 0; count_row < n; ++count_row)
+		for (unsigned int count_column = 0; count_column < m; ++count_column)
 			Array[count_row][count_column] = (rand() % 100); 
 																							  // âûâîä ìàññèâà
-	for (int count_row = 0; count_row < n; count_row++)
+	for (unsigned int count_row = 0; count_row < n; ++count_row)
 	{
-		for (int count_column = 0; count_column < m; count_column++)
+		for (unsigned int count_column = 0; count_column < m; ++count_column)
 			cout << setw(4) << setprecision(2) << Array[count_row][count_column] << "   ";
 		cout << endl;
 	}
 
-	for (int count_row = 0; count_row < n; count_row++)
+	for (int count_row = 0; count_row < n; ++count_row)
 	{
 		int sum = 0;
-		for (int count_column = 0; count_column< m; count_column++)
+		for (unsigned int count_column = 0; count_column< m; ++count_column)
 		{
 			sum += Array[count_row][count_column];
 		}
 		cout << "Summary of elements of row["<<count_row<<"] = " << sum<<endl;
 	}
 
-	
-
-
-	
-
-	
 	for (int count = 0; count < n; count++)
 		delete[]Array[count];
-	system("pause");
+	_getch();
 	return 0;
 }
